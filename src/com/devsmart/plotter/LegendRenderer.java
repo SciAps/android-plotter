4a136de7552e29d867fbb98b683bf4514086c210
Unknown <tnguyen@sciaps.com>
2017-12-20 12:54:18 -0500

    legend renderer

--- src/com/devsmart/plotter/LegendRenderer.java
+++ src/com/devsmart/plotter/LegendRenderer.java
@@ -1,5 +1,15 @@
 package com.devsmart.plotter;
 
+/**
+ * Created by timnguyen on 12/20/17.
+ */
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+import java.util.List;
+
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -7,7 +17,7 @@ import android.graphics.RectF;
 
 import java.util.List;
 
-public final class LineWithTypeRenderer implements DataRenderer {
+public final class LegendRenderer implements DataRenderer {
     public static class XYPair {
         public float x;
         public float y;
@@ -23,53 +33,43 @@ public final class LineWithTypeRenderer implements DataRenderer {
         }
     }
 
-    private final List<XYPair> mLinesFromOriginList;
     private final Paint mPaint = new Paint();
+    private int mColor;
+    private int mYPosition;
+    private String mTitle;
 
     private final int LEGEND_PADDING_LEFT = 55;
     private final int LEGEND_DIMENSION = 15;
     private final int TEXT_PADDING_LEFT = 50;
 
-    public LineWithTypeRenderer(List<XYPair> linesFromOriginList, String lineType, int yPosition) {
-        mLinesFromOriginList = linesFromOriginList;
+    public LegendRenderer(String title, int color, int yPosition) {
+        mColor = color;
+        mPaint.setColor(color);
         mPaint.setStrokeWidth(1.0f);
         mPaint.setAntiAlias(true);
         mPaint.setStyle(Paint.Style.STROKE);
+        mYPosition = yPosition;
+        mTitle = title;
     }
 
     @Override
     public void draw(Canvas canvas, RectF viewPort, CoordinateSystem coordSystem) {
+        mPaint.setColor(mColor);
         mPaint.setStrokeWidth(1.0f);
         mPaint.setAntiAlias(true);
         mPaint.setStyle(Paint.Style.STROKE);
 
-        float[] point = new float[2];
         float[] origin = {0, 0};
         coordSystem.mapPoints(origin);
-        for (XYPair xyPair : mLinesFromOriginList) {
 
-            mPaint.setColor(xyPair.mColor);
-            point[0] = xyPair.x;
-
-            coordSystem.mapPoints(point);
-            canvas.drawLine(point[0], origin[1], point[0], canvas.getHeight()- 50f, mPaint);
-
-            if (xyPair.value != null) {
-                canvas.save();
-                canvas.scale(1, -1);
-
-                mPaint.setTextSize(20);
-                mPaint.setStyle(Paint.Style.FILL);
-
-                int shiftPixelLeft = 10;
-                if (xyPair.value.length() == 1) {
-                    shiftPixelLeft = 5;
-                }
-                canvas.drawText(xyPair.value + " (" + xyPair.typeOfLine + ")", point[0]-shiftPixelLeft, -(canvas.getHeight()- 50f), mPaint);
-
-                canvas.restore();
-            }
-        }
+        mPaint.setStrokeWidth(3);
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(canvas.getWidth()-LEGEND_DIMENSION-LEGEND_PADDING_LEFT, canvas.getHeight()-mYPosition, canvas.getWidth()-LEGEND_PADDING_LEFT, canvas.getHeight()-LEGEND_DIMENSION-mYPosition, mPaint);
+        canvas.save();
+        canvas.scale(1, -1);
+        mPaint.setTextSize(20);
+        canvas.drawText(mTitle, canvas.getWidth()-TEXT_PADDING_LEFT, -(canvas.getHeight()-LEGEND_DIMENSION-mYPosition), mPaint);
+        canvas.restore();
     }
 
     @Override
